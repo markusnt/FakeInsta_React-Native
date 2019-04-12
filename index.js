6bdb02d7a4c3709f93bf9818623f5750d0328262
markusnt <markus_nathan@live.com>
2019-04-12 15:30:30 -0300

    dependence navigation v.2

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ import Feed from './src/screens/Feed';
 import Login from './src/screens/Login.js';
 
 AppRegistry.registerComponent(appName, () => Login);
+
+
+
+// https://www.youtube.com/watch?v=8SO-UCHurVc
+// ASSISTIR ESSE VIDEO PARA INSTALAR O REACT NATIVE NAVIGATION V.2
